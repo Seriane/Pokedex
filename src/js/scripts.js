e9f6ca4cb3d37a3d99854802db6c3fe8b4f30fcf
Nizar HDADECH <hdadech.nizar@gmail.com>
2017-03-02 04:00:18 +0100

    store

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Details from "./components/Details";
+import Informations from "./components/Informations";
 
 import Layout from "./components/Layout";
+import Main from "./components/Main";
+import Store from "./store"
+const app = document.getElementById("app");
 
-const app = document.getElementsByClassName('container')[0];
+ReactDOM.render(
+	<Provider store={Store} >
+		<Router history = {hashHistory} >
+			<Route path = "/" component = {Layout} >
+				<IndexRoute component = {Main} ></IndexRoute>
+				<Route path = "informations" component = {Informations} >
+				</Route>
+				<Route path = "details/:id/:identifier" component={Details}></Route>
 
-ReactDOM.render(<Layout/>,app);
+			</Route>
+		</Router>
+	</Provider>,app);
